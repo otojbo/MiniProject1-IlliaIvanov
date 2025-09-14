a7fbe4e486645d1d7150a979df13a61ae68b221b
otojbo <fepiks@gmail.com>
2025-09-14 16:22:24 -0500

    feat: added imports and list of stock
    Signed-off-by: otojbo <fepiks@gmail.com>

---
+++ main.py
@@ -0,0 +1,10 @@
+# INF 601 - Advanced Python
+# Illia Ivanov
+# Mini Project 1
+
+import numpy as np
+import matplotlib.pyplot as plt
+import yfinance as yf
+from pathlib import Path
+
+STOCKS = ["AAPL","MSFT","GOOGL","AMZN","TSLA"]
